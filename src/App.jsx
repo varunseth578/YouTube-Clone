d53e07662a9f086a6099830f16910ddc18ab0d30
Varun Seth <122156664+varunseth578@users.noreply.github.com>
2024-07-29 21:22:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Provider } from "react-redux";
 import "./App.css";
 import Body from "./Body";
 import Head from "./Head";
-import store from "./utils.js/store"
+import store from "./utils/store"
 
 function App() {
   return (
